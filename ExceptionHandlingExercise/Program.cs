b74e8253a3fe57ef54850c30d5323be6b550c97b
Michael Driscoll <michael.driscoll951@gmail.com>
2023-06-02 12:17:59 -0700

    hw

--- ExceptionHandlingExercise/Program.cs
+++ ExceptionHandlingExercise/Program.cs
@@ -21,15 +21,38 @@ namespace ExceptionHandlingExercise
 
 
             //TODO START HERE:
+
+            // Make a foreach loop to iterate through your character array------DONE
+            var arr = new char[] { '1', '2', '3', '4', '5', '6','a','b','c' };
+            var numbers = new List<int>();
+            var str = "";
+            foreach(var item in arr)
+            {
+                try
+                {
+                    str = item.ToString();
+                    var number = int.Parse(str);
+                    numbers.Add(number);
+                }
+                catch(Exception e)
+                {
+                    Console.WriteLine($"unable to parse {item}");
+                }
+            }
+                foreach(var num in numbers)
+                {
+                    Console.WriteLine(num);
+                }
             
-            // Make a foreach loop to iterate through your character array
+       
+            // Now create a try catch---------DONE
             
-                // Now create a try catch
-                
+
+            
                 
                     // Inside your try block
                         // set your string variable to each array element in your char[] to .ToString()
-                        // Now, using int.Parse, parse your string variable and store in an int variable
+                        // Now, using, parse your string variable and store in an int variable int.Parse
                         // Then add each int to your list
                 
                 // catch your Exception:
